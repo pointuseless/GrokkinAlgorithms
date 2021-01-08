ef5ac6e3302c1dcdc98cad238aa3a03628d40cd8
pointuseless <s3m.channel@gmail.com>
2021-01-08 18:21:40 +0200

    binsearch completed

--- DivideAndConquer/Recursion/binsearch.py
+++ DivideAndConquer/Recursion/binsearch.py
@@ -1,5 +1,21 @@
 from typing import Optional, Union
 
 
-def binary_search(term: Union[int, float, str], seq: list[Union[int, float, str]]) -> Optional[int]:
-    pass
+def binary_search(term: Union[int, float, str],
+                  seq: list[Union[int, float, str]]) -> Optional[int]:
+
+    if not seq:
+        return None     # Base Case 1
+
+    mid = (len(seq) - 1) // 2
+    mid_item = seq[mid]
+
+    if mid_item == term:
+        return mid      # Base Case 2
+    elif mid_item > term:
+        return binary_search(term, seq[:mid])
+    elif mid_item < term:
+        return binary_search(term, seq[mid + 1:]) + mid + 1
+
+
+print(binary_search('f', ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']))
